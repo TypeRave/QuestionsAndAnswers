f62b6e12b702e0adc40112c671b5e0fce126b2d1
Brandon Gomez <bgomez9212@gmail.com>
2023-09-01 18:46:12 -0700

    add routes, change some datatypes for schema

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require("express");
-const questionRoute = require("./src/routes.js")
+const router = require("./src/routes.js")
 const fs = require("fs")
 const app = express()
 const port = 3000;
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.use('/qa', router)
 
-app.use('/questions', questionRoute)
 app.listen(port, () => console.log('listening on port 3000'))
