f915f8135d0fb746b4c097dccc8a7e36e160cc86
Brandon Gomez <bgomez9212@gmail.com>
2023-09-07 13:47:49 -0700

    add verification for load.io

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 require('dotenv').config();
 const router = require("./src/routes.js")
-
-const { PORT } = process.env;
+const path = require("path");
 const app = express()
+const { PORT } = process.env;
+const fs = require('fs');
 
-console.log(PORT)
+const fp = path.join(__dirname, './verification.txt')
+
+app.use('loaderio-23bada7c6291776da2a75f6bd2c418c4', (req, res) =>{
+  let body = fs.readFileSync(fp);
+  res.send(body);
+})
+
+
 app.use(express.json())
-
+app.use('/', (req, res) => res.send('connected to server'))
 app.use('/qa', router)
 
 app.listen(PORT, () => console.log('listening on port 3000'))
