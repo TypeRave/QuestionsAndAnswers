30036d42ea689a6ef147a7b603b6a8a42b165620
Brandon Gomez <bgomez9212@gmail.com>
2023-09-06 21:41:24 -0700

    add ability to post questions and get answers

--- test.sql
+++ test.sql
@@ -2,3 +2,7 @@ SELECT questions.question_id, answers.answer_id, answers_photos.id FROM question
 LEFT JOIN answers ON answers.question_id = questions.question_id
 LEFT JOIN answers_photos ON answers_photos.answer_id = answers.answer_id
 WHERE questions.product_id = 1 AND questions.reported = false
+
+SELECT * FROM answers
+LEFT JOIN answers_photos ON answers_photos.answer_id = answers.answer_id
+where answers.question_id = ${question_id} AND reported = false
